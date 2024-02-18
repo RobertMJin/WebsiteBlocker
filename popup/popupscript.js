3096fc37f5da5d0d4ff8f68d814f2bcae637819c
RobertMJin <robert-jin@outlook.com>
2024-02-18 12:39:57 -0500

    switch to bulma, update css

--- popup/popupscript.js
+++ popup/popupscript.js
@@ -1,15 +1,30 @@
-const startTime = document.getElementById("startTime");
-const endTime = document.getElementById("endTime");
 //on change of startTime/endTime, need to send info to Content.js
-startTime.addEventListener(
-  "input",
-  () => {
-  },
-  false,
-);
-startTime.addEventListener(
-  "input",
-  () => {
-  },
-  false,
-);
+document.addEventListener(DOMContentLoaded, function () {
+  const startTime = document.getElementById("startTime");
+  startTime.addEventListener('change', function () {
+    //do something here
+  });
+});
+
+//on change of startTime/endTime, need to send info to Content.js
+document.addEventListener(DOMContentLoaded, function () {
+  const endTime = document.getElementById("endTime");
+  endTime.addEventListener('change', function () {
+    //do something here
+  });
+});
+
+document.addEventListener(DOMContentLoaded, function () {
+  const endTime = document.getElementById("endTime");
+  endTime.addEventListener('change', function () {
+    //do something here
+  });
+});
+
+
+document.addEventListener(DOMContentLoaded, function () {
+  const pauseTime = document.getElementById("pauseTime");
+  pauseTime.addEventListener('change', function () {
+    //do something here
+  });
+});
